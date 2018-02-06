7ea69d760c4279b5592c70cfbbbdd9cf19a959c7
andrea.avallone <andrea.avallone@CPX-0EZSJ95LJMQ.dir.svc.accenture.com>
2018-02-06 18:07:17 +0100

    .

---
+++ dgs-backend/src/main/java/com/accenture/dsg/backend/utils/MailSend.java
@@ -0,0 +1,44 @@
+//package com.accenture.dsg.backend.utils;
+//
+//import org.springframework.mail.MailException;
+//import org.springframework.mail.MailSender;
+//import org.springframework.mail.SimpleMailMessage;
+//
+//public class MailSend {
+//
+//    private MailSender mailSender;
+//    private SimpleMailMessage templateMessage;
+//
+//    public void setMailSender(MailSender mailSender) {
+//        this.mailSender = mailSender;
+//    }
+//
+//    public void setTemplateMessage(SimpleMailMessage templateMessage) {
+//        this.templateMessage = templateMessage;
+//    }
+//
+//    public void placeOrder(Order order) {
+//
+//        // Do the business calculations...
+//
+//        // Call the collaborators to persist the order...
+//
+//        // Create a thread safe "copy" of the template message and customize it
+//        SimpleMailMessage msg = new SimpleMailMessage(this.templateMessage);
+//        msg.setTo(order.getCustomer().getEmailAddress());
+//        msg.setText(
+//            "Dear " + order.getCustomer().getFirstName()
+//                + order.getCustomer().getLastName()
+//                + ", thank you for placing order. Your order number is "
+//                + order.getOrderNumber());
+//        try{
+//            this.mailSender.send(msg);
+//        }
+//        catch (MailException ex) {
+//            // simply log it and go on...
+//            System.err.println(ex.getMessage());
+//        }
+//    }
+//
+//}
+//
