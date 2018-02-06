fe262910a2bd8ddd40fd4f8cb2f264bfb6662c48
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-06 13:57:13 +0100

    .

---
+++ dgs-backend/src/main/java/com/accenture/dsg/backend/dao/AnswerCrudRepository.java
@@ -0,0 +1,10 @@
+package com.accenture.dsg.backend.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
+import com.accenture.dsg.backend.model.Answer;
+@Component
+public interface AnswerCrudRepository extends CrudRepository<Answer, Long>{
+
+}
