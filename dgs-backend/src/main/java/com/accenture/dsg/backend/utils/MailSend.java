34899e40b4d4eae28384bd4cc540909af11d8aca
andrea.avallone <andrea.avallone@CPX-0EZSJ95LJMQ.dir.svc.accenture.com>
2018-02-06 18:14:52 +0100

    .

--- dgs-backend/src/main/java/com/accenture/dsg/backend/utils/MailSend.java
+++ dgs-backend/src/main/java/com/accenture/dsg/backend/utils/MailSend.java
@@ -1,44 +1,33 @@
-//package com.accenture.dsg.backend.utils;
-//
-//import org.springframework.mail.MailException;
-//import org.springframework.mail.MailSender;
-//import org.springframework.mail.SimpleMailMessage;
-//
-//public class MailSend {
-//
-//    private MailSender mailSender;
-//    private SimpleMailMessage templateMessage;
-//
-//    public void setMailSender(MailSender mailSender) {
-//        this.mailSender = mailSender;
-//    }
-//
-//    public void setTemplateMessage(SimpleMailMessage templateMessage) {
-//        this.templateMessage = templateMessage;
-//    }
-//
-//    public void placeOrder(Order order) {
-//
-//        // Do the business calculations...
-//
-//        // Call the collaborators to persist the order...
-//
-//        // Create a thread safe "copy" of the template message and customize it
-//        SimpleMailMessage msg = new SimpleMailMessage(this.templateMessage);
-//        msg.setTo(order.getCustomer().getEmailAddress());
-//        msg.setText(
-//            "Dear " + order.getCustomer().getFirstName()
-//                + order.getCustomer().getLastName()
-//                + ", thank you for placing order. Your order number is "
-//                + order.getOrderNumber());
-//        try{
-//            this.mailSender.send(msg);
-//        }
-//        catch (MailException ex) {
-//            // simply log it and go on...
-//            System.err.println(ex.getMessage());
-//        }
-//    }
-//
-//}
-//
+package com.accenture.dsg.backend.utils;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class MailSend {
+
+    private MailSender mailSender;
+
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String Body,String subject,String Bcc,String Cc,String To) {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(To);
+        msg.setCc(Cc);
+        msg.setBcc(Bcc);
+        msg.setSubject(subject);
+        msg.setText(Body);
+        try{
+            this.mailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+
+}
+
