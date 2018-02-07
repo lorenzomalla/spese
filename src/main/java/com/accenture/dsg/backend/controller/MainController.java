5edd7630a4dc27fecafdbfd4ed063218b9bae58c
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-07 17:42:13 +0100

    .

--- src/main/java/com/accenture/dsg/backend/controller/MainController.java
+++ src/main/java/com/accenture/dsg/backend/controller/MainController.java
@@ -86,11 +86,11 @@ public class MainController {
 	}
 	
 	@GetMapping(path="/getNode")
-	public @ResponseBody String getNextNode(@RequestParam int treeId) {
+	public @ResponseBody String getNextNode(@RequestParam Long treeId) {
 		// This returns a JSON or XML with the users
 		JsonObject respObj = new JsonObject();
 		System.out.println("---------------------------> START");
-		List<TreeStructure> Trees = treeC.findByParentId((long) treeId);
+		List<TreeStructure> Trees = treeC.findByParentId(treeId);
 		System.out.println("---------------------------> JSON");
 		respObj.add("Tree", (JsonElement) Trees);
 		System.out.println("---------------------------> TOSTRING");
--- src/main/java/com/accenture/dsg/backend/model/TreeStructure.java
+++ src/main/java/com/accenture/dsg/backend/model/TreeStructure.java
@@ -20,7 +20,7 @@ import java.util.Set;
 @Table(name="tree_structure")
 @NamedQueries({
 	@NamedQuery(name="TreeStructure.findAll", query="SELECT t FROM TreeStructure t"),
-	@NamedQuery(name="TreeStructure.findByParentId", query="SELECT t FROM TreeStructure t WHERE t.treeStructure = :treeId")
+	@NamedQuery(name="TreeStructure.findByParentId", query="SELECT t FROM TreeStructure t WHERE t.treeStructure.parent_id = :treeId")
 })
 public class TreeStructure implements Serializable {
 	private static final long serialVersionUID = 1L;
