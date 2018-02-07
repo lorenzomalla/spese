27651eda8a022e1bd0dac040dd02a3d4bdf001c6
federico.tortora <federico.tortora@accenture.com>
2018-02-07 18:00:36 +0100

    navigation

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
-	for(i=0; i<3; i++) {
-		createCard(i, "","Domanda "+i,"Risposta"+i);
-	}
+	history.pushState = function(state) {
+		change(state);
+		return original.apply(this, arguments);
+	};
+
+	$(window).on("popstate", function(e) {
+        change(e.originalEvent.state);
+    });
+
+	randomCards();
 });
+
+function change(state) {
+	randomCards();
+//    if(state === null) {
+//         $("div").text("Original"); 
+//    } else {
+//        $("div").text(state.url);
+//    }
+}
 
 function createCard(id, icon, title, description) {
 	$("#cardList").append("<div class=\"card-column\" data-node-id=\""+id+"\">"+
@@ -18,8 +34,19 @@ function createCard(id, icon, title, description) {
 			            "</div>");
 	$(".card-column").click(function() {
 		var id = $(this).data("id");
-		alert(id);
+		history.pushState({ url: "/" }, "/", "?path="+id);
+//		alert(id);
 	});
-//	$(".card-column:eq(0)").addClass("offset-md-3");
-//	$(".card-column:not(:eq(0))").addClass("offset-md-0");
+}
+
+//remove in prod
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+}
+function randomCards() {
+	for(i=0; i<getRandomInt(0, 5); i++) {
+		createCard(i, "","Domanda "+i,"Risposta"+i);
+	}
 }
