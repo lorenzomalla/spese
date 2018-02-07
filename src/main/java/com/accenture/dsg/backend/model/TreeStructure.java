09d7700b1e58b88fb774ff50525e1a319ce4c04b
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-07 18:41:38 +0100

    .

--- src/main/java/com/accenture/dsg/backend/model/TreeStructure.java
+++ src/main/java/com/accenture/dsg/backend/model/TreeStructure.java
@@ -20,7 +20,7 @@ import java.util.Set;
 @Table(name="tree_structure")
 @NamedQueries({
 	@NamedQuery(name="TreeStructure.findAll", query="SELECT t FROM TreeStructure t"),
-	@NamedQuery(name="TreeStructure.findByParentId", query="SELECT t FROM TreeStructure t JOIN t.parent_id p WHERE p.id = :treeId")
+	@NamedQuery(name="TreeStructure.findByParentId", query="SELECT t, a, q FROM TreeStructure t JOIN t.parent_id p JOIN t.answers a JOIN  t.questions q WHERE p.id = :treeId")
 })
 public class TreeStructure implements Serializable {
 	private static final long serialVersionUID = 1L;
