80e1e40e12b7bd3634f03700064ed057dc4835c4
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-08 11:05:25 +0100

    .

--- src/main/java/com/accenture/dsg/backend/dao/TreeCrudRepository.java
+++ src/main/java/com/accenture/dsg/backend/dao/TreeCrudRepository.java
@@ -1,13 +1,15 @@
 package com.accenture.dsg.backend.dao;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 import com.accenture.dsg.backend.model.TreeStructure;
 
 @Component
-public interface TreeCrudRepository extends CrudRepository<TreeStructure, Long>{
+public interface TreeCrudRepository extends JpaRepository<TreeStructure, Long>{
 
 	List<TreeStructure> findByParentId(@Param("treeId") Long treeId);
 }
