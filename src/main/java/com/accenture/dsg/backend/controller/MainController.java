cd3e864b8f2f27059a46c5d5f80997ccbffdb32d
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-08 11:04:31 +0100

    .

--- src/main/java/com/accenture/dsg/backend/controller/MainController.java
+++ src/main/java/com/accenture/dsg/backend/controller/MainController.java
@@ -2,6 +2,7 @@ package com.accenture.dsg.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,8 +32,8 @@ public class MainController {
 
 	
 	@RequestMapping(value={"/","/home"}, method = RequestMethod.GET)
-	public ModelAndView home(){
-		return new ModelAndView("login");
+	public String home(){
+		return "login";
 	}
 	
 	@RequestMapping(value={"/error"}, method = RequestMethod.GET)
