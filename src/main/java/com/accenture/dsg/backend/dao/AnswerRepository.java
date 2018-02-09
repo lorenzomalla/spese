c16ff9331f1eb9eda5236ff6050a9ab89d6c029e
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-09 12:30:03 +0100

    .

---
+++ src/main/java/com/accenture/dsg/backend/dao/AnswerRepository.java
@@ -0,0 +1,11 @@
+package com.accenture.dsg.backend.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.accenture.dsg.backend.model.Answer;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+}
