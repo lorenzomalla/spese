676ad2f57e363225dc425a7ab18fa469ab7e7c7f
lorenzo.mallardo <lorenzo.mallardo@CPX-JTDXT9P8VL2.dir.svc.accenture.com>
2018-02-09 11:14:58 +0100

    UPD

--- src/main/java/com/accenture/dsg/backend/controller/MainController.java
+++ src/main/java/com/accenture/dsg/backend/controller/MainController.java
@@ -1,12 +1,15 @@
 package com.accenture.dsg.backend.controller;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -148,4 +151,23 @@ public class MainController {
 		}
 	    	return "Inviata Correttamente";
 	}
+	
+	 @Bean
+	    public JavaMailSender getJavaMailSender() {
+	        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	        mailSender.setHost("smtp.gmail.com");
+	        mailSender.setPort(587);
+
+	        mailSender.setUsername("**EMAIL**");
+	        mailSender.setPassword("**PASSWORD**");
+
+	        Properties props = mailSender.getJavaMailProperties();
+	        props.put("mail.transport.protocol", "smtp");
+	        props.put("mail.smtp.auth", "true");
+	        props.put("mail.smtp.starttls.enable", "true");
+	        props.put("mail.debug", "true");
+
+	        return mailSender;
+	    }
+	
 }
