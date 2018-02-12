0401f3e4375e54052c6e7114fd8e455bf29ca1c7
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-12 18:39:49 +0100

    .

--- src/main/java/com/accenture/dsg/backend/dao/TreeStructureDaoImpl.java
+++ src/main/java/com/accenture/dsg/backend/dao/TreeStructureDaoImpl.java
@@ -35,7 +35,7 @@ public class TreeStructureDaoImpl implements TreeStructureDao{
 		TreeStructure tree = null;
 		try{
 			tree = (TreeStructure) em.createQuery("SELECT t FROM TreeStructure t LEFT OUTER JOIN fetch t.questions q "
-					+ "LEFT OUTER JOIN fetch t.answers a LEFT OUTER JOIN fetch t.template templ "
+					+ "LEFT OUTER JOIN fetch t.answers a LEFT OUTER JOIN fetch t.templates templ "
 					+ " WHERE t.id = '"+id+"'").getSingleResult();
 		}catch(Exception e){
 			System.err.println("Errore");
@@ -48,7 +48,7 @@ public class TreeStructureDaoImpl implements TreeStructureDao{
 	public TreeStructure getTree() {
 		TreeStructure tree = null;
 		try{
-			tree = (TreeStructure) em.createQuery("SELECT t FROM TreeStructure t LEFT OUTER JOIN t.answers a LEFT OUTER JOIN a.template templ LEFT OUTER JOIN t.questions q "
+			tree = (TreeStructure) em.createQuery("SELECT t FROM TreeStructure t LEFT OUTER JOIN t.answers a LEFT OUTER JOIN a.templates templ LEFT OUTER JOIN t.questions q "
 					+ " WHERE t.treeStructure.id IS NULL").getSingleResult();
 			// AND a.treeStructure.id = t.id AND a.template.id = templ.id AND q.treeStructure.id = t.id
 		}catch(Exception e){
