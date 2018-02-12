381d63185289e4beadfc9bf29448f90280b9f2b6
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-12 17:55:50 +0100

    .

--- src/main/java/com/accenture/dsg/backend/dao/TreeStructureDaoImpl.java
+++ src/main/java/com/accenture/dsg/backend/dao/TreeStructureDaoImpl.java
@@ -36,8 +36,8 @@ public class TreeStructureDaoImpl implements TreeStructureDao{
 		try{
 			tree = (TreeStructure) em.createQuery("SELECT t FROM TreeStructure t LEFT OUTER JOIN fetch t.questions q "
 					+ "LEFT OUTER JOIN fetch t.answers a "
-					+ "LEFT OUTER JOIN a.template templ ON templ.id = a.template.id"
-					+ "LEFT OUTER JOIN templ.catTemplate ct ON ct.id = templ.catTemplate.id"
+					+ "JOIN a.template templ ON templ.id = a.template.id"
+					+ "JOIN templ.catTemplate ct ON ct.id = templ.catTemplate.id"
 					+ " WHERE t.id = '"+id+"'").getSingleResult();
 		}catch(Exception e){
 			System.err.println("Errore");
