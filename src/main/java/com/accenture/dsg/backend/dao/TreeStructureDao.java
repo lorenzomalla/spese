1aed6ddf2c27dd9fc26af0e8349c420abb0b33d5
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-12 15:00:31 +0100

    .

--- src/main/java/com/accenture/dsg/backend/dao/TreeStructureDao.java
+++ src/main/java/com/accenture/dsg/backend/dao/TreeStructureDao.java
@@ -3,11 +3,12 @@ package com.accenture.dsg.backend.dao;
 import java.util.List;
 
 import com.accenture.dsg.backend.model.TreeStructure;
+import com.accenture.dsg.backend.utils.ResponseObjectWrapper;
 
 public interface TreeStructureDao{
 	
 	public List<TreeStructure> getAllLista();
-	public TreeStructure getFindById(int id);
+	public ResponseObjectWrapper getFindById(int id);
 	public TreeStructure getTree();
 	public void persistTreeStructure(TreeStructure tree);
 
