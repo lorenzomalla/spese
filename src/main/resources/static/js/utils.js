c1319efcbde2a1e61e11e08223cd9be237ef8a49
lorenzo.mallardo <lorenzo.mallardo@CPX-JTDXT9P8VL2.dir.svc.accenture.com>
2018-02-15 17:45:47 +0100

    .

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -3,35 +3,55 @@ function select2init() {
 	$('.js-data-example-ajax').select2(
 			{
 			width: '100%',	
+//			ajax: {
+//				delay: 500,
+//				url: '/findOptions',
+//				type: "GET",
+//				dataType: "json",
+//				data: function (params) {
+//					var query = {
+//						search: params.term,
+//						type: 'public'
+//					}
+//					console.log(query);
+//					return query;
+//				},
+//				processResults: function (data) {
+//					console.log(data);
+//					var opt = '{"results":['; 
+//					$.each(data,function(key,value){ 
+//						console.log("VALORE : " + value);
+//						opt +='{"id":"'+value.value+'","text":"'+value.oOption+'","phone":"'+value.phone+'","mail":"'+value.email+'"},';
+//					});
+//					opt = opt.substring(0, opt.length-1);
+//					opt +=']}'; 
+//					console.log(opt);
+//					return opt;
+//				}
+//			},
 			ajax: {
-				delay: 500,
-				url: '/findOptions',
-				type: "GET",
-				dataType: "json",
-				data: function (params) {
-					var query = {
-						search: params.term,
-						type: 'public'
-					}
-					console.log(query);
-					return query;
-				},
-				processResults: function (data) {
-					console.log(data);
-					var opt = '{"results":['; 
-					$.each(data,function(key,value){ 
-						console.log("VALORE : " + value);
-						opt +='{"id":"'+value.value+'","text":"'+value.oOption+'","phone":"'+value.phone+'","mail":"'+value.email+'"},';
-					});
-					opt = opt.substring(0, opt.length-1);
-					opt +=']}'; 
-					console.log(opt);
-					return opt;
-				}
-			},
+		        url: "/findOptions",
+		        dataType: 'json',
+		        delay: 250,
+		        data: function (params) {
+		            return {
+		                q: params.term // search term
+		            };
+		        },
+		        processResults: function (data) {
+		            // parse the results into the format expected by Select2.
+		            // since we are using custom formatting functions we do not need to
+		            // alter the remote JSON data
+		            return {
+		                results: data
+		            };
+		        },
+		        cache: true
+		    },
+		    minimumInputLength: 2
 			theme:'classic',
 			allowClear:true,
-			minimumInputLength: 0
+//			minimumInputLength: 0
 		}).on('select2:select',function(e){
 		    var data=e.params.data;
 		    if(data.id!=null){
