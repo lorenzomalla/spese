171ad58fc322f0bb97a5ad8cf31263467ce1771b
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-15 15:35:37 +0100

    upd js

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -1,55 +1,61 @@
 var redirecturl='';
-$(document).ready(function(){
-	var opt = "<option></option>";
-	$.ajax({
-		type: "GET",
-		url: "/findOptions",
-		ContentType: "application/json",
-		dataType: "json",
-		success: function(response){
-			var select = $('#singleselect');
-			$.each(response,function(key,pippo){
-				//Prende l'oggetto 
-			var pippo2 =	$.parseJSON(pippo);
-				console.log("VALORE : " + pippo2);
-				opt +="<option value="+pippo2.value+">"+pippo2.option+"</option>";
-			});
-			select.html(opt);
-			select.show();
-			opt = "";
-		},
-		error: function(){
-			console.log("Errore nella richiesta");
-		}
-	});
-});
+//$(document).ready(function(){
+//	var opt = "<option></option>";
+//	$.ajax({
+//		type: "GET",
+//		url: "/findOptions",
+//		ContentType: "application/json",
+//		dataType: "json",
+//		success: function(response){
+//			var select = $('#singleselect');
+//			$.each(response,function(key,pippo){
+//				//Prende l'oggetto 
+//			var pippo2 =	$.parseJSON(pippo);
+//				console.log("VALORE : " + pippo2);
+//				opt +="<option value="+pippo2.value+">"+pippo2.option+"</option>";
+//			});
+//			select.html(opt);
+//			select.show();
+//			opt = "";
+//		},
+//		error: function(){
+//			console.log("Errore nella richiesta");
+//		}
+//	});
+//});
 
 $('.js-data-example-ajax').select2(
-		{theme:'classic',placeholder:'Seleziona un valore dalla lista...',
+	{
+		theme:'classic',
+		placeholder:'Seleziona un valore dalla lista...',
 		allowClear:true,
-//		ajax: {
-//			url: 'https://dgs-backend.herokuapp.com/findOptions',
-//			data: function (params) {
-//				var query = {
-////					search: params.term,
-////					type: 'public'
-//				}
-//				console.log(query);
-//				return query;
-//			},
-//			processResults: function (data) {
-//				console.log(data);
-//				return {
-//					results: data.items
-//				};
-//			},
-//			transport: function (params, success, failure) {
-//				var $request = $.ajax(params);
-//				$request.then(success);
-//				$request.fail(failure);
-//				return $request;
-//			}
-//		}
+		ajax: {
+			type: "GET",
+			url: "/findOptions",
+			ContentType: "application/json",
+			dataType: "json",
+			data: function (params) {
+				var query = {
+//					search: params.term,
+//					type: 'public'
+				}
+				console.log(query);
+				return query;
+			},
+			processResults: function (data, param) {
+				console.log(data);
+				console.log(param);
+				return {
+					results: data.items
+				};
+			},
+			transport: function (params, success, failure) {
+				var $request = $.ajax(params);
+				$request.then(success);
+				$request.fail(failure);
+				return $request;
+			}
+		}
 	}).on('select2:select',function(e){
 	    var data=e.params.data;
 	    if(data.id!=null){
