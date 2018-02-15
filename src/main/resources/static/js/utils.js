117b672fe0f82a0159b20c892f5780d321711e64
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-15 12:25:00 +0100

    update select2 js and home

---
+++ src/main/resources/static/js/utils.js
@@ -0,0 +1,39 @@
+var redirecturl='';
+$('.js-example-basic-single').select2
+({theme:'classic',placeholder:'...',
+    allowClear:true,
+    ajax: {
+    url: 'https://dgs-backend.herokuapp.com/findOptions',
+    data: function (params) {
+      var query = {
+        search: params.term,
+        type: 'public'
+      }
+      return query;
+    }
+    processResults: function (data) {
+      return {
+        results: data.items
+      };
+    }
+    }
+})
+.on('select2:select',function(e){
+    var data=e.params.data;
+    if(data.id!=null){
+        document.redirecturl = data.id;
+        $('#button-select').prop('disabled',false);
+        }else{
+            $('#button-select').prop('disabled',true);
+        }
+        console.log(document.redirecturl);
+    }
+);
+$('#button-select').click(function(){
+    if(document.redirecturl=='https://dgs-backend.herokuapp.com/?path=6'){
+         window.location.href = document.redirecturl;
+    }else{
+         window.open(document.redirecturl, '_blank');
+    } 
+});
+ 	
