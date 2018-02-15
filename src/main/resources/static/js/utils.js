26d92ceb82378b45ca06d6e2ef44a681b20709e4
lorenzo.mallardo <lorenzo.mallardo@CPX-JTDXT9P8VL2.dir.svc.accenture.com>
2018-02-15 15:08:25 +0100

    .

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -1,31 +1,52 @@
 var redirecturl='';
+$(document).ready(function(){
+	$.ajax({
+		type: "GET",
+		url: "/findOptions",
+		ContentType: "application/json",
+		dataType: "json",
+		success: function(response){
+			var select = $('select2:select');
+			$.each(response,function(key,valore){
+				//Prende l'oggetto 
+//			JSON.stringify(value);
+				opt +="<option value="+valore.value+">"+valore.option+"</option>";
+			});
+			select.html(opt);
+			select.show();
+			opt = "";
+		},
+		error: function(){
+			console.log("Errore nella richiesta");
+		}
+});
 
 $('.js-data-example-ajax').select2(
 		{theme:'classic',placeholder:'Seleziona un valore dalla lista...',
 		allowClear:true,
-		ajax: {
-			url: 'https://dgs-backend.herokuapp.com/findOptions',
-			data: function (params) {
-				var query = {
-//					search: params.term,
-//					type: 'public'
-				}
-				console.log(query);
-				return query;
-			},
-			processResults: function (data) {
-				console.log(data);
-				return {
-					results: data.items
-				};
-			},
-			transport: function (params, success, failure) {
-				var $request = $.ajax(params);
-				$request.then(success);
-				$request.fail(failure);
-				return $request;
-			}
-		}
+//		ajax: {
+//			url: 'https://dgs-backend.herokuapp.com/findOptions',
+//			data: function (params) {
+//				var query = {
+////					search: params.term,
+////					type: 'public'
+//				}
+//				console.log(query);
+//				return query;
+//			},
+//			processResults: function (data) {
+//				console.log(data);
+//				return {
+//					results: data.items
+//				};
+//			},
+//			transport: function (params, success, failure) {
+//				var $request = $.ajax(params);
+//				$request.then(success);
+//				$request.fail(failure);
+//				return $request;
+//			}
+//		}
 	}).on('select2:select',function(e){
 	    var data=e.params.data;
 	    if(data.id!=null){
