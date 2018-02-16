b2fad99902c4ac09c3c88178ff38c98d351544b8
andrea.avallone <andrea.avallone@fwc11401.net.accenturetechnologysolutions.it>
2018-02-16 16:17:07 +0100

    .

--- src/main/java/com/accenture/dsg/backend/model/Contact.java
+++ src/main/java/com/accenture/dsg/backend/model/Contact.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Table(name="contatti")
 @NamedQueries({
 	@NamedQuery(name="Contact.findAll", query="SELECT c FROM Contact c"),
-	@NamedQuery(name="Contact.getByRef", query="SELECT c FROM Contact c WHERE c.branch= :branch AND c.option= :option")
+	@NamedQuery(name="Contact.getByRef", query="SELECT c FROM Contact c WHERE c.branch= :branch AND c.option= :option"),
 })
 public class Contact implements Serializable {
 	private static final long serialVersionUID = 1L;
