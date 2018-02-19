501aff4189ba3de12aab43eb64f74e9e0feacdbb
lorenzo.mallardo <lorenzo.mallardo@CPX-JTDXT9P8VL2.dir.svc.accenture.com>
2018-02-19 10:23:01 +0100

    .

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -72,6 +72,21 @@ $(document).ready(function() {
 		
 	getCards();
 	
+	$('#continue').click(function(event){
+		controllConditionCheck(event);
+		validazione();
+		if(!$('#form-registrazione').valid()){
+			$('#email').css("border-color", "#FF0000");
+			$('#codiceFiscale').css("border-color", "#FF0000");
+			$('#problema').css("border-color", "#FF0000");
+		}else{
+			$('#email').css("border-color", "#ced4da");
+			$('#codiceFiscale').css("border-color", "#ced4da");
+			$('#problema').css("border-color", "#ced4da");
+		}
+	});
+
+	
 	$("#cardList").on("click", ".card-column", function(event) {
 		event.stopPropagation();
 		var id = $(this).data("node-id");
@@ -191,5 +206,44 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
 }
+
+function validazione(){
+	$.validator.addMethod("regx", function(value, element, regexpr) {          
+	    return regexpr.test(value);
+	});
+	$('#form-registrazione').validate({
+		rules:{
+			codiceFiscale:{
+				required:true,
+				maxlength : 15
+			},
+			email:{
+				required:true,
+				email: true
+			}
+		},
+			messages:{
+				codiceFiscale:{
+					required:"",
+					maxlength: ""
+				},
+				email:{
+					required : "",
+					email: ""
+				},
+			}
+	});
+}
+function controllConditionCheck(){
+	var selValue = $('input[name=policy]:checked').val(); 
+	if(selValue == "no"){
+		event.preventDefault();
+		alert("Per favore prima di proseguire accetta le condizioni");
+//		$('#policy').css("border-color", "#FF0000");
+		return false;
+	}else
+		return true;
+}
+
 
 
