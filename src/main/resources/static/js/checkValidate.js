ae67bb87632e533557434784301e47b2a6768ada
federico.tortora <federico.tortora@accenture.com>
2018-02-20 19:14:25 +0100

    fix position radio error

--- src/main/resources/static/js/checkValidate.js
+++ src/main/resources/static/js/checkValidate.js
@@ -1,7 +1,7 @@
 function formValidation() {
 	var isValid = false;
 	validazione();
-	if(grecaptcha.getResponse()!="") {
+	if (grecaptcha.getResponse() != "") {
 		$.ajax({
 			async : false,
 			type : "POST",
@@ -21,64 +21,72 @@ function formValidation() {
 			error : function(response) {
 				isValid = false;
 			}
-		});		
+		});
 	} else {
-		isValid=false;
+		isValid = false;
 	}
-//	if ($('#form-registrazione').valid()) {
-//		validazione();
-//		if ($('#email').val() != "") {
-//			$('#email').css("border-color", "#ced4da");
-//		} else {
-//			$('#email').css("border-color", "#FF0000");
-//		}
-//		if ($('#codiceFiscale').val() != "") {
-//			$('#codiceFiscale').css("border-color", "#ced4da");
-//		} else {
-//			$('#codiceFiscale').css("border-color", "#FF0000");
-//		}
-//		if ($('#problema').val() != "") {
-//			$('#problema').css("border-color", "#ced4da");
-//		} else {
-//			$('#problema').css("border-color", "#FF0000");
-//		}
-//	}
+	// if ($('#form-registrazione').valid()) {
+	// validazione();
+	// if ($('#email').val() != "") {
+	// $('#email').css("border-color", "#ced4da");
+	// } else {
+	// $('#email').css("border-color", "#FF0000");
+	// }
+	// if ($('#codiceFiscale').val() != "") {
+	// $('#codiceFiscale').css("border-color", "#ced4da");
+	// } else {
+	// $('#codiceFiscale').css("border-color", "#FF0000");
+	// }
+	// if ($('#problema').val() != "") {
+	// $('#problema').css("border-color", "#ced4da");
+	// } else {
+	// $('#problema').css("border-color", "#FF0000");
+	// }
+	// }
 	return isValid && $('#form-registrazione').valid();
 }
 
 function validazione() {
-	$.validator.addMethod("regx", function(value, element, regexpr) {          
-	    return regexpr.test(value);
+	$.validator.addMethod("regx", function(value, element, regexpr) {
+		return regexpr.test(value);
 	});
-	$('#form-registrazione').validate({
-		rules : {
-			codiceFiscale : {
-				required : true,
-				regx: /(^[A-Z]{6}[0-9]{2}[A-Z]{1}[0-9]{2}[A-Z]{1}[0-9]{3}[A-Z]{1})|(^[0-9]{11})$/i
-			},
-			email : {
-				required : true,
-				email : true
-			},
-			privacyPolicy: {
-				required: true,
-				equalTo: "#privacy-yes"
-			}
-		},
-		messages : {
-			codiceFiscale : {
-				required : "Campo obbligatorio.",
-				regx : "Inserire una Partita Iva o un Codice Fiscale corretto."
-			},
-			email : {
-				required : "Campo obbligatorio.",
-				email : "L'email non è in un formato corretto."
-			},
-			privacyPolicy: {
-				required : "Campo obbligatorio.",
-				equalTo: "Accettare il trattamento."
-			}
-		}
-	});
+	$('#form-registrazione')
+			.validate(
+					{
+						errorPlacement : function(error, element) {
+							if (element.attr("name") == "privacyPolicy") {
+								error.insertAfter($("#privacyPolicyYes"));
+							} else {
+								error.insertAfter(element);
+							}
+						},
+						rules : {
+							codiceFiscale : {
+								required : true,
+								regx : /(^[A-Z]{6}[0-9]{2}[A-Z]{1}[0-9]{2}[A-Z]{1}[0-9]{3}[A-Z]{1})|(^[0-9]{11})$/i
+							},
+							email : {
+								required : true,
+								email : true
+							},
+							privacyPolicy : {
+								required : true,
+								equalTo : "#privacy-yes"
+							}
+						},
+						messages : {
+							codiceFiscale : {
+								required : "Campo obbligatorio.",
+								regx : "Inserire una Partita Iva o un Codice Fiscale corretto."
+							},
+							email : {
+								required : "Campo obbligatorio.",
+								email : "L'email non è in un formato corretto."
+							},
+							privacyPolicy : {
+								required : "Campo obbligatorio.",
+								equalTo : "Accettare il trattamento."
+							}
+						}
+					});
 }
-
